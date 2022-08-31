0639932baf13e70f2ca5a1afc0aebea1645340fa
opilane <opilane@HP-CZC6297BBL>
2022-08-31 11:41:50 +0300

    Add project files.

---
+++ KordamineOOP/Koer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KordamineOOP
+{
+    class Koer : Koduloom
+    {
+        public enum toug { Mops, Foxtier };
+        public toug Toug;
+        public Koer(toug Toug, string nimi, string varv, sugu loomaSugu, double kaal = 0, int vanus = 0, bool elav = false) : base(nimi, varv, loomaSugu, kaal, vanus, elav)
+        {
+        }
+        public Koer(Koer koer)
+        {
+            this.Toug = koer.Toug;
+            this.nimi = koer.nimi;
+            this.varv = koer.varv;
+            this.loomaSugu = koer.loomaSugu;
+            this.kaal = koer.kaal;
+            this.vanus = koer.vanus;
+            this.elav = koer.elav;
+        }
+        public override void print_Info()
+        {
+            Console.WriteLine("{0}{1}{2} ta on {3} ja tema kaal on {4} ja ta on {5} aastat vana", Toug, varv, nimi, loomaSugu, kaal, vanus);
+        }
+        public override void print_Haal()
+        {
+            Console.WriteLine("aaa");
+        }
+        public void muudaNimi(string uusNimi) { nimi = uusNimi; }   
+        public void muudaVarv(string uusVarv) { varv = uusVarv; }
+        public void muudaSugu(sugu uusSugu) { loomaSugu = uusSugu; }
+        public void muudaKaal(double uusKaal) { kaal = uusKaal; }
+        public void muudaVanus(int uusVanus) {vanus = uusVanus; }
+        public void muudaElav(bool uusElav) { elav=uusElav; }
+
+    }
+}
---
+++ KordamineOOP/Program.cs
@@ -0,0 +1,14 @@
+﻿using KordamineOOP;
+
+
+Koer koer = new Koer(Koer.toug.Mops, " Kiot", " punane ", Koduloom.sugu.isane, 35.5, 7, true);
+koer.print_Info();
+koer.print_Haal();
+Koer veelkoer = new Koer(koer);
+veelkoer.muudaNimi("Murka");
+veelkoer.muudaVarv("kollane");
+veelkoer.muudaSugu(Koduloom.sugu.isane);
+veelkoer.muudaKaal(31.1);
+veelkoer.muudaVanus(3);
+veelkoer.muudaElav(true);
+veelkoer.print_Info();

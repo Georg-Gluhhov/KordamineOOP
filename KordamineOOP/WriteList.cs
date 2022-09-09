2b3cf9522b6675bfaac8dc797e6b4259ca976f38
opilane <opilane@HP-CZC6297BBL>
2022-09-09 08:46:15 +0300

    1234

--- KordamineOOP/WriteList.cs
+++ KordamineOOP/WriteList.cs
@@ -55,68 +55,63 @@ namespace KordamineOOP
                     list.Clear();
                 }
             }
+            Koer koer = new Koer(Koer.toug.Mops, " ", " ", Koduloom.sugu.isane, 0, 0, true);
+            Hiir hiir = new Hiir(18, " ", " ", Koduloom.sugu.isane, 0, 0, true);
 
-            int g = 0;
+           int g = 0;
             foreach (String s in BigList)
             {
                 if (g == 6) { g = 0; }
-                if (s == $"Koer ") {Koer  koer = new  Koer(Koer.toug.Mops, " ", " ", Koduloom.sugu.isane, 0, 0, true);
-                    Split -= 1;
-                }
-                else if( s==$"Hiir") { Hiir hiir = new Hiir(18, " ", " ", Koduloom.sugu.isane, 0, 0, true);
-                    Split -= 1;
-                }
                 if (s == "Koer" || g > 0 && g !=6 ) { 
                 switch (s)
-                {
-                        case var _ when s.Contains("Koer"):
-                            Koer koer = new Koer(Koer.toug.Mops, " ", " ", Koduloom.sugu.isane, 0, 0, true);
-                            break;    
+                {    
                         case var _ when s.Contains("Kasvu on"):
-                            Console.WriteLine(s);
+                            koer.muudaVarv(s.Replace(" Kasvu on ", ""));
                             break;
                         case var _ when s.Contains("varv on"):
                             koer.muudaVarv(s.Replace(" varv on ", ""));
                             break;     
                         case var _ when s.Contains("nimi on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" nimi on ", ""));
                             break;
                         case var _ when s.Contains("sugu on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" sugu on ", ""));
                             break;
                         case var _ when s.Contains("kaal on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" kaal on ", ""));
                             break;
                         case var _ when s.Contains("vanus on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" vanus on ", ""));
                             break;
                 }
                     g++;
+                    koer.print_Info();
                 }
                 else if (s == "Hiir" || g > 0 && g != 6)
                 {
                     switch (s)
                     {
                         case var _ when s.Contains("Kasvu on"):
-                            Console.WriteLine(s);
+                            koer.muudaVarv(s.Replace(" Kasvu on ", ""));
                             break;
                         case var _ when s.Contains("varv on"):
-                            Console.WriteLine(s);
+                            koer.muudaVarv(s.Replace(" varv on ", ""));
                             break;
                         case var _ when s.Contains("nimi on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" nimi on ", ""));
                             break;
                         case var _ when s.Contains("sugu on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" sugu on ", ""));
                             break;
                         case var _ when s.Contains("kaal on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" kaal on ", ""));
                             break;
                         case var _ when s.Contains("vanus on"):
-                            Console.WriteLine(s);
+                            koer.muudaNimi(s.Replace(" vanus on ", ""));
                             break;
                     }
                     g++;
+                    hiir.print_Info();
                 }
 
             }
